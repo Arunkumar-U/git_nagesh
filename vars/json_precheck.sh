38e890760ef417eeef34fc896bc6e870ade5a80b
arunkumar16111999 <pnl0r86j@Emea.Royalahold.net>
2024-07-25 22:59:38 +0530

    Validating the json file

--- vars/json_precheck.sh
+++ vars/json_precheck.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# env setting
+path='/home/runner/work/git_nagesh/git_nagesh/templates/ctmjobs/acc'
+
 # Validate JSON file
-jsonlint templates/ctmjobs/acc/CONTROL-M_JAVA_ACC.json
+jsonlint "${path}"/CONTROL-M_JAVA_ACC.json
 
 # Check the previous exit status of jsonlint
 if [ $? -eq 0 ]; then
